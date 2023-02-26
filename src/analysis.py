3158f7494385f0829d8aa51d2a09d9969c4befc1
imalanz <arq.imalanz@gmail.com>
2023-02-26 13:14:01 +0100

    sunday

--- src/analysis.py
+++ src/analysis.py
@@ -13,11 +13,14 @@ import requests
 from dotenv import load_dotenv
 import pymongo
 
-# adding a column with the year and in the rows the cuantity of production in ton.
-def adding_yearcolumns (df, year):
-    # iteration to create a dictionary getting the values in the column year.
-    _1999 = {index:row[4] for index, row in df.iterrows() if row[2] == year}
-    df1999 = pd.DataFrame.from_dict(_1999, orient="index")
-    df1999.rename(columns= {0: 1999}, inplace = True)
-    # return df.
-    return pd.concat([df, df1999], axis = 1)
+# PRODUCTION.ipynb
+
+def iterate_column_years (year):
+    # iterate 2010
+    x = {index:[row[6],row[0],row[2]] for index, row in production.iterrows() if row[4] == year}
+    df = pd.DataFrame.from_dict(x, orient="index")
+    df.rename(columns= {0: year, 1:"Area", 3:"Item"}, inplace = True)
+    df["Item"] = df["Item"].astype(object)
+    df["Area"] = df["Area"].astype(object)
+    df[year] = df[year].astype(object)
+    return df
